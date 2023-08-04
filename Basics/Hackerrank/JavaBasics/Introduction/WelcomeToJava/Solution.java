0219e12bc72ce34dae83bf90d61926fa18c34657
abhinavmaharana <abhinavmaharana.004@gmail.com>
2023-08-05 01:45:43 +0530

    Java Basic Practice

---
+++ Basics/Hackerrank/JavaBasics/Introduction/WelcomeToJava/Solution.java
@@ -0,0 +1,11 @@
+// Print Hello World
+
+package Basics.Hackerrank.JavaBasics.Introduction.WelcomeToJava;
+
+public class Solution {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
