d041ad21dcc3fa72bcba67c3d20895a0bc77a21e
abhinavmaharana <abhinavmaharana.004@gmail.com>
2023-08-05 14:41:24 +0530

    Hackerrank Coding Practice

---
+++ Basics/Hackerrank/JavaBasics/Introduction/JavaStaticInitializerBlock/Solution.java
@@ -0,0 +1,22 @@
+package Basics.Hackerrank.JavaBasics.Introduction.JavaStaticInitializerBlock;
+import java.util.*;
+public class Solution {
+    public static boolean flag = true;
+    public static int B;
+    public static int H;
+    static {
+
+        Scanner sc = new Scanner (System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+
+        if (B<0 || H<0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        if (B==0 && H==100) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+}
