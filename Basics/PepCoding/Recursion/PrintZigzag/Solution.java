ab45db2c9219dea033f8e52055ac00b2b5420d12
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-01-04 23:35:14 +0530

    Recursion practice.

---
+++ Basics/PepCoding/Recursion/PrintZigzag/Solution.java
@@ -0,0 +1,22 @@
+package Basics.PepCoding.Recursion.PrintZigzag;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if (n == 0) {
+            return;
+         }
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+    }
+}
