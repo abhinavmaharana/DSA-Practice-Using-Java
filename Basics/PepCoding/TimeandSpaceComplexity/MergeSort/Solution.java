797069a922195ef5e3655b4251f0042352530061
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-01-20 17:02:42 +0530

    fix

--- Basics/PepCoding/TimeandSpaceComplexity/MergeSort/Solution.java
+++ Basics/PepCoding/TimeandSpaceComplexity/MergeSort/Solution.java
@@ -5,9 +5,15 @@ public class Solution {
         return null;
     }
 
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
     public static int[] mergeTwoSortedArrays(int[] a, int[] b) {
         System.out.println("Merging these two arrays");
         System.out.println("left array -> ");
         print(a);
+        return a;
     }
 }
